7ec3af277876dcbc9bd0fa91bd411469a59dd406
Rene Osman <osmanra2@users.noreply.github.com>
2017-07-14 11:56:51 -0700

    Upload initial files
    Initial files

---
+++ lightbox.js
@@ -0,0 +1,43 @@
+$(document).ready(function($) {
+
+    $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'http://localhost:8888/lightbox/lightbox.css') );
+
+    $('body').append('<p><a href="#ct" class="lightbox_trigger">Lightbox Example</a></p>');
+
+    $('.lightbox_trigger').click(function(e) {
+
+        //prevent default action (hyperlink)
+        e.preventDefault();
+
+
+
+        if ($('#lightbox').length > 0) {
+
+
+            $('#content').html('<div class="ctbox">Lightbox example</div>');
+
+            //show lightbox window - you could use .show('fast') for a transition
+            $('#lightbox').show();
+        }
+
+        else {
+
+            var ct =
+                '<div id="lightbox">' +
+                '<p>Click to close</p>' +
+                '<div id="content">' + //insert clicked link's href into img src
+                '<div id="ctbox">Lightbox example</div>' +
+                '</div>' +
+                '</div>';
+
+            //insert lightbox HTML into page
+            $('body').append(ct);
+        }
+
+    });
+
+    $(document).on('click', '#lightbox', function() {
+        $('#lightbox').hide();
+    });
+
+});
